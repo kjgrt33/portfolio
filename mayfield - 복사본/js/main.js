91f3494c442f468552f8dcee0a6db1e43ad8c5d2
jhj <kjgrt33@naver.com>
2023-08-22 17:57:35 +0900

    last

--- mayfield - 복사본/js/main.js
+++
@@ -1,7 +0,0 @@
-// menu 버튼 움직임 스크립트
-document.getElementById("menu").onclick = function () {
-  document.getElementsByTagName("span")[0].classList.toggle("bar1");
-  document.getElementsByTagName("span")[1].classList.toggle("bar2");
-  document.getElementsByTagName("span")[2].classList.toggle("bar3");
-  document.getElementsByTagName("nav")[0].classList.toggle("nav_bg");
-};
