d7bb9cb23080f9407e4fc171602d26fc385502f3
jhj <kjgrt33@naver.com>
2023-08-25 16:35:03 +0900

    last

--- mayfield/js/main.js
+++ mayfield/js/main.js
@@ -5,3 +5,34 @@ document.getElementById("menu").onclick = function () {
   document.getElementsByTagName("span")[2].classList.toggle("bar3");
   document.getElementsByTagName("nav")[0].classList.toggle("nav_bg");
 };
+
+// visual
+/*var swiper1 = new Swiper(".mySwiper1", {
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});*/
+
+// TOP 버튼
+window.onscroll = function () {
+  let ht = document.documentElement.scrollTop;
+  console.log(ht);
+  if (ht > 50) {
+    document.getElementsByClassName("btn_top")[0].style.opacity = "1";
+  } else {
+    document.getElementsByClassName("btn_top")[0].style.opacity = "0";
+  }
+};
+document.getElementsByClassName("btn_top")[0].onclick = function () {
+  document.documentElement.scrollTop = "0";
+};
