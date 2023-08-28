6babe8233d035d859b374b2495807c7ab1767c8d
jhj <kjgrt33@naver.com>
2023-08-28 10:22:40 +0900

    first

--- mayfield/js/main.js
+++ mayfield/js/main.js
@@ -7,15 +7,29 @@ document.getElementById("menu").onclick = function () {
 };
 
 // visual
+/*var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      effect: "fade",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });*/
+
 var swiper1 = new Swiper(".mySwiper1", {
+  spaceBetween: 30,
   effect: "fade",
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
-    type: "fraction",
+    clickable: true,
   },
 });
 
