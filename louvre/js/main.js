7e30d1dc56be710ac91d8d3982a4a10043b9a0c4
jhj <kjgrt33@naver.com>
2023-09-13 18:00:24 +0900

    last

--- louvre/js/main.js
+++ louvre/js/main.js
@@ -1,10 +1,15 @@
 // IN THE NEWS
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
-  spaceBetween: 30,
+  spaceBetween: 20,
   centeredSlides: true,
+  //loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
