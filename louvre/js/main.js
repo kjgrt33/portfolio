b9931eaf2d4db87c9a9470f43c75a07e4202a264
jhj <kjgrt33@naver.com>
2023-09-18 14:12:00 +0900

    first

--- louvre/js/main.js
+++ louvre/js/main.js
@@ -1,9 +1,25 @@
 // IN THE NEWS
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  // spaceBetween: 20,
-  // centeredSlides: true,
-  // loop: true,
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: "auto",
+  //loop: true,
+  spaceBetween: 20,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// LOUVRE AT HOME
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: "auto",
+  //loop: true,
+  spaceBetween: 20,
+  centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
