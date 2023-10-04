93a53a3268d24c7da1761a6eabce8fe8b638c117
jhj <kjgrt33@naver.com>
2023-10-04 17:24:40 +0900

    last

--- my/js/main.js
+++ my/js/main.js
@@ -13,33 +13,52 @@ $(function () {
     let i = $(this).parent().index();
     $("html, body").animate({ scrollTop: arr[i] - 60 }, 1000);
   });
-});
 
-//popup
-//열기
-$("#popup_view").click(function (e) {
-  e.preventDefault();
-  $("#popup").css({ display: "block" });
-  $("body").css({ overflow: "hidden" });
-});
+  //popup
+  //열기
+  $(".portfolio .con li a").click(function () {
+    let i = $(this).parent().index();
+    if (i >= 3) {
+      $("#popup00 > div").css({ display: "none" });
+      $("#popup00 > div")
+        .eq(i - 3)
+        .css({ display: "block" });
+      $("body").css({ overflow: "hidden" });
+      return false;
+    }
+  });
+  // 나중에 클론페이지 없이 각 리스트마다 레이어팝업 뜨게 할때 활용!!!
+  /* $(".portfolio .con li a").click(function (e) {
+    e.preventDefault();
+    let i = $(this).parent().index();
+    
+      $("#popup00 > div").css({ display: "none" });
+      $("#popup00 > div")
+        .eq(i)
+        .css({ display: "block" });
+      $("body").css({ overflow: "hidden" });
+   
+  }); */
 
-//닫기
-$("#close").click(function (e) {
-  e.preventDefault();
-  $("#popup").css({ display: "none" });
-  $("body").css({ overflow: "visible" });
-});
+  //닫기
+  $(".btn_close").click(function (e) {
+    e.preventDefault();
+    $("#popup00 > div").css({ display: "none" });
+    $("body").css({ overflow: "visible" });
+  });
 
-// TOP 버튼
-window.onscroll = function () {
-  let ht = document.documentElement.scrollTop;
-  console.log(ht);
-  if (ht > 1000) {
-    document.getElementsByClassName("btn_top")[0].style.opacity = "1";
-  } else {
-    document.getElementsByClassName("btn_top")[0].style.opacity = "0";
-  }
-};
-document.getElementsByClassName("btn_top")[0].onclick = function () {
-  document.documentElement.scrollTop = "0";
-};
+  // TOP 버튼
+  window.onscroll = function () {
+    let ht = document.documentElement.scrollTop;
+    console.log(ht);
+    if (ht > 1000) {
+      document.getElementsByClassName("btn_top")[0].style.opacity = "1";
+    } else {
+      document.getElementsByClassName("btn_top")[0].style.opacity = "0";
+    }
+  };
+  $(".btn_top").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate({ scrollTop: "0" }, 1000);
+  });
+});
